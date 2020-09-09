f54c011ed4a8dd482459f3e32c97283e5ad6e8b3
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-09-09 11:14:57 -0300

    removi os testes que não funcionaram

--- nodejs/configs/axios.config.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const data = axios.create({
-  baseURL:
-    "https://books.google.com.br/books?id=xxoXcuh0oS0C&hl=pt-BR&pg=PA3&jscmd=click3",
-});
-export default { data };
--- nodejs/index.ts
+++
@@ -1,5 +0,0 @@
-import api from "./configs/axios.config";
-
-let response = api.data("").then((resp) => {
-  console.log(resp.data);
-});
